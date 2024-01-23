9b46a21e86a4c93b6b07aad0aa7fb5650898f536
michaelcfoti <michael-christopher.foti.1@ens.etsmtl.ca>
2024-01-23 09:20:10 -0500

    modif

---
+++ RnB_v01/make_figure.m
@@ -0,0 +1,46 @@
+function make_figure(R,W,S,name)
+        figure('position',[400 260 800 340],'menu','none','name','RnB');
+        colR = [161 22 50]/256;
+        % Average spectral analysis of rhytmics signals
+        subplot(1,2,2)
+        hold on; ax0 = gca;
+        plot(log(R.fr),smooth(R.pw),'-k','LineWidth',2,'color',colR);
+        setupAxes(ax0,true,log([0.3 64]), []); 
+        Y = 1.1*max(smooth(R.pw));
+        text(log(0.47),Y,'RnB Spectroscopy','FontSize',14,'FontAngle','italic','FontWeight','bold');
+        text(log(8),Y,sprintf('(%d epochs)',size(S,1)),'FontSize',14);
+        xlabel('Frequency (Hz)','FontSize',16);
+        ylabel('Rhythmic Power Spectral Density (dB)','FontSize',14);
+        ylim([0 Y]);
+        % Beta distribution
+        subplot(1,2,1)
+        hold on; ax = gca; ax.Color = 'none';
+        [a,b] = hist(W.betas,60);
+        p = a/sum(a) / (b(2)-b(1));
+        plot(b,p,'.');
+        b = b(2:end-1);
+       
+        % Gaussina fit (here, one gaussian)
+        Gauss = Best_Betas_Gaussian_fit(W.betas');
+        plot(Gauss.x,Gauss.y,'-','LineWidth',2,'Color',colR);
+        % print the modes:
+        Y = 1.25*max(Gauss.y);
+        X = 0.1+min(Gauss.x);
+        text(X,Y,sprintf('Mixture of %d Gaussian(s):',numel(Gauss.mu)),...
+            'FontSize',14,'HorizontalAlignment','left','FontWeight','bold')
+        for u = 1:numel(Gauss.mu)
+        text(X,(1-u*0.07)*Y,sprintf('beta = %3.1f (%3.1f%%)',Gauss.mu(u),100*Gauss.prop(u)),...
+            'FontSize',14,'HorizontalAlignment','left','FontAngle','italic');
+        end
+        plot([2 2],[0 0.7*max(Gauss.y)],':k','LineWidth',2);
+        xlabel('Spectral exponent $\beta$','FontSize',20,'Interpreter','latex','FontWeight','bold')
+        ylabel(name,'FontSize',22,'FontWeight','bold')
+        ylim([0 1.25*max(Gauss.y)])
+        hold off
+        
+fprintf('\n    The left figure displays the distribution of the scaling\n')
+fprintf('    exponent across the epochs. The curve is an optimal fit\n')
+fprintf('    (in terms of an optimal Akaike Index Criteria) with a mixture\n')
+fprintf('    of Gaussians. The right figure shows the ''RnB spectroscopy''\n')
+fprintf('    of the %s during NREM sleep.\n',name)
+end
--- RnB_wavelet_main.m
+++ RnB_wavelet_main.m
@@ -3,44 +3,38 @@ function RnB_wavelet_main()
 clc; clear; close all;
 restoredefaultpath;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Integrate RnB_v01 library
+% librairy RnB
 addpath(genpath('./RnB_v01'), 'data')
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Example (320 epochs, Hippocampus)                            
-load('R37_N3.mat')
-[sR, pW] = wRnB_extract_Rhythmic_signals(data_epochs,'alpha',4,'J', 8);
-pR      = RnB_compute_spectre_brut(sR,fs);                             
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fprintf('\n -- RnB Algorithm:\n')
+fprintf('    This code applies the ''rhythmic and\n')
+fprintf('    arrhythmic'' decomposition of a batch\n')
+fprintf('    of 20 seconds epochs.\n')
+fprintf('    The data are extracted from the Atlas\n')
+fprintf('    database (van Ellenrieder et al., 2019)\n\n')
 
-    % Display
-    figure('position',[400 160 800 340]);
-    colR = [161 22 50]/256;
-    
-    % Average spectral analysis of rhytmics signals
-    subplot(1,2,2)
-    hold on; ax0 = gca;
-    plot(log(pR.fr),smooth(pR.pw),'-k','LineWidth',2,'color',colR);
-    setupAxes(ax0,true,log([1 64]), []);
-    text(log(2),1.8,sprintf('%d epochs',size(sR,1)),'FontSize',14);
-    xlabel('Frequency (Hz)','FontSize',16);
-    ylabel('Rhythmic Power Spectral Density (dB)','FontSize',14);
-    
-    % Beta distribution
-    subplot(1,2,1)
-    hold on; ax = gca; ax.Color = 'none';
-    [a,b] = hist(pW.betas,60);
-    p = a/sum(a) / (b(2)-b(1));
-    plot(b,p,'.');
-    b = b(2:end-1);
-    plot([2 2],[0 3.2],':k','LineWidth',2);
-    plot(b,smooth(p(2:end-1),7),'-','LineWidth',2,'Color',colR);
-    plot([pW.beta pW.beta],[0 3],'--k','LineWidth',2);
-    text(pW.beta,3.2,sprintf('beta = %3.1f [%3.1f,%3.1f] ',pW.beta, pW.bMin, pW.bMax),'FontSize',18,'HorizontalAlignment','center','FontAngle','italic')
-    xlabel('$\beta$','FontSize',20,'Interpreter','latex')
-    ylabel(region,'FontSize',22,'FontWeight','bold')
-    hold off
-    
+    dbs = dir('data/R*');
+    fprintf('    Available data are:\n')
+    for i = 1:numel(dbs)
+        load([dbs(i).folder filesep dbs(i).name],'region');
+        fprintf('    . Region %d: %s\n',i,region)
+    end
+    index = input('    Select a region index: ');
+    load([dbs(index).folder filesep dbs(index).name])
+
+fprintf('\n    You selected: %s\n',region)
+fprintf('\t . %d epochs\n',size(data_epochs,1))
+fprintf('\t . %d samples (%4.2f seconds @ %4.2f Hz)\n\n',size(data_epochs,2),size(data_epochs,2)/fs,fs);
+pause(0.75);
+fprintf('(epoch,total = %d,beta exponent)\n',size(data_epochs,1));
+pause(0.75);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                         
+[sR, pW] = wRnB_extract_Rhythmic_signals(data_epochs,'alpha', 4,'J', 8);
+pR      = RnB_compute_spectre_brut(sR,fs);                     
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Display 
+make_figure(pR,pW,sR,region);
+fprintf('\nBye.\n')
 end
