7106bd8f4bf12ba9b3a8f68066b203df5e7adb9c
michaelcfoti <michael-christopher.foti.1@ens.etsmtl.ca>
2024-06-21 18:50:11 -0400

    update

--- wRnBmain/wRnB.m
+++ wRnBmain/wRnB.m
@@ -8,18 +8,16 @@ addpath(genpath('./RnB_v01'), 'data')
 
 load('R37_N3.mat');
 
-fprintf('\n    You selected: %s\n',region)
+fprintf('\n    Data:\n');
 fprintf('\t . %d epochs\n',size(data_epochs,1))
 fprintf('\t . %d samples (%4.2f seconds @ %4.2f Hz)\n\n',size(data_epochs,2),size(data_epochs,2)/fs,fs);
 pause(0.75);
-fprintf('(epoch,total = %d,beta exponent)\n',size(data_epochs,1));
-pause(0.75);
-fprintf('\nProcessing...\n');
+fprintf('\nProcessing...\n\n');
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                         
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                            
 [sR, pW] = wRnB_extract_Rhythmic_signals(data_epochs,'alpha', 4,'J', 8, 'betaScales', [1,9]);
 pR      = RnB_compute_spectre_brut(sR,fs);                     
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 
 % Save 
 createDirectory('results');
