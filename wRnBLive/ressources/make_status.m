2f24a508b7d4efb40522e6b8d826bb0260795234
michaelcfoti <michael-christopher.foti.1@ens.etsmtl.ca>
2025-05-30 07:34:20 -0400

    removing live script

--- wRnBLive/ressources/make_status.m
+++
@@ -1,7 +0,0 @@
-function make_status(region,data_epochs,fs)
-fprintf('\n    You selected %s:\n',region)
-fprintf('\t . %d epochs\n',size(data_epochs,1))
-fprintf('\t . %d samples (%4.2f seconds @ %4.2f Hz)\n',size(data_epochs,2),size(data_epochs,2)/fs,fs);
-fprintf('\t . The wRnB will take max. %2.1f minutes.\n',size(data_epochs,1)/180);
-fprintf('\t . To proceed, press the next button ''wavelet-RnB'' ... and be patient ...');
-end
